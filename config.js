ca9f5b2ee3f7f4d1b68880cd03cc747c23bd78ca
VK SER <89385674+Vk-ser@users.noreply.github.com>
2021-08-28 21:17:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 /* Copyright (C) 2020 Yusuf Usta.
-Reedited by hisham & Ameer
+
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
@@ -26,7 +26,6 @@ module.exports = {
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
     GANSTYLE: process.env.GAN_IMAGE === undefined ? 'https://i.hizliresim.com/loUtAb.jpg' : process.env.GAN_IMAGE,
     LANG: process.env.LANGUAGE === undefined ? 'TR' : process.env.LANGUAGE.toUpperCase(),
-    OWNER: process.env.OWNER_NAME === undefined ? 'default' : process.env.OWNER_NAME,
     ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
     KICKMEMSG: process.env.KICKME_MESSAGE === undefined ? 'default' : process.env.KICKME_MESSAGE,
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
@@ -38,8 +37,8 @@ module.exports = {
     DISSTICKER: process.env.DISABLE_STICKER === undefined ? false : process.env.DISABLE_STICKER,
     NOLOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
     FULLEVA: process.env.FULL_EVA === undefined ? 'false' : process.env.FULL_EVA,
-    GIF_WEL: process.env.GIF_WEL === undefined ? 'https://github.com/Vk-ser/pathumma/raw/master/pathummagif.mp4' : process.env.GIF_WEL,
-    GIF_BYE: process.env.GIF_BYE === undefined ? 'https://github.com/Vk-ser/pathumma/raw/master/pathumma123.mp4' : process.env.GIF_BYE,
+    GIF_WEL: process.env.GIF_WEL === undefined ? 'https://i.imgur.com/I6zw2j9.mp4' : process.env.GIF_WEL,
+    GIF_BYE: process.env.GIF_BYE === undefined ? 'https://i.imgur.com/NTar8dQ.mp4' : process.env.GIF_BYE,
     BLOCKMSG: process.env.BLOCK_MESSAGE === undefined ? 'default' : process.env.BLOCK_MESSAGE,
     UNBLOCKMSG: process.env.UNBLOCK_MESSAGE === undefined ? 'default' : process.env.UNBLOCK_MESSAGE,
     UNMUTEMSG: process.env.UNMUTE_MESSAGE === undefined ? 'default' : process.env.UNMUTE_MESSAGE,
